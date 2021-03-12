271bb644b2fda9e44a97788520ea1959f58f3b83
flacnut <flacnut@github.com>
2021-03-11 23:10:25 -0800

    [Feature] Create CSV Parser and cli for ingesting bank statement csv's.

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,68 @@
 import "module-alias/register";
-import startServer from "./server";
-
 import yargs from "yargs";
 
-console.dir(process.argv.slice(2));
+import startServer from "./server";
+import { BasicCSVParser } from "./cli/BasicCSVParser";
 
 yargs(process.argv.slice(2))
+  .scriptName("collateral-cli")
+  .usage("$0 <cmd> [args]")
   .command(
     "start-server",
     "Starts the graphQL and Web server",
     (_) => {},
     (_) => {
       startServer();
+    }
+  )
+  .command(
+    "import",
+    "Imports a file into the database",
+    (yargs) => {
+      yargs
+        .usage(
+          `Example:\nimport --file testFile.csv --parser csv -d "Posting Date" -a "Amount" --desc "Extended Description"`
+        )
+        .option("f", {
+          alias: "file",
+          description: "The file to be imported",
+          demandOption: true,
+        })
+        .option("p", {
+          alias: "parser",
+          description: "The parser to use",
+          choices: ["csv"],
+          default: "csv",
+        })
+        .option("d", {
+          alias: "date-column",
+          description: "The column heading for the date value.",
+          demandOption: true,
+        })
+        .option("a", {
+          alias: "amount-column",
+          description: "The column heading for the amount value.",
+          demandOption: true,
+        })
+        .option("desc", {
+          alias: "desc-column",
+          description: "The column heading for the transaction description.",
+          demandOption: true,
+        });
+    },
+    async (argv: {
+      dateColumn: string;
+      amountColumn: string;
+      descColumn: string;
+      file: string;
+    }) => {
+      const parser = new BasicCSVParser({
+        date: argv.dateColumn,
+        amount: argv.amountColumn,
+        description: argv.descColumn,
+      });
+      const transactions = await parser.parse(argv.file);
+      console.dir(transactions);
     }
   )
   .demandCommand(1)
