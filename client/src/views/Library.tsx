eb2747d13f92f3ffa13158dadc14660f71e11d95
flacnut <adam.sheldon@hotmail.com>
2021-04-04 22:18:09 -0700

    [Hack] Get dropzone working on library page.

--- client/src/views/Library.tsx
+++ client/src/views/Library.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useCallback } from "react";
+import { useDropzone } from "react-dropzone";
 
 import { makeStyles, createStyles, Theme } from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
@@ -7,6 +8,8 @@ import Grid from "@material-ui/core/Grid";
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {
+      padding: 30,
+      paddingTop: 80,
       flexGrow: 1,
     },
     paper: {
@@ -17,11 +20,43 @@ const useStyles = makeStyles((theme: Theme) =>
   })
 );
 
+function MyDropzone() {
+  const onDrop = useCallback((acceptedFiles: File[]) => {
+    // Do something with the files
+    acceptedFiles.forEach((file: File) => {
+      const reader = new FileReader();
+
+      reader.onabort = () => console.log("file reading was aborted");
+      reader.onerror = () => console.log("file reading has failed");
+      reader.onload = () => {
+        // Do whatever you want with the file contents
+        const binaryStr = reader.result;
+        console.dir(binaryStr?.toString());
+      };
+      reader.readAsArrayBuffer(file);
+    });
+  }, []);
+
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+
+  return (
+    <div {...getRootProps()}>
+      <input {...getInputProps()} />
+      {isDragActive ? (
+        <p>Drop the files here ...</p>
+      ) : (
+        <p>Drag 'n' drop some files here, or click to select files</p>
+      )}
+    </div>
+  );
+}
+
 export default function Library() {
   const classes = useStyles();
 
   return (
     <div className={classes.root}>
+      <MyDropzone />
       <Grid container spacing={3}>
         <Grid item xs={12}>
           <Paper className={classes.paper}>xs=12</Paper>
