e8bead521224cb17c296886de4a59169c5292439
flacnut <adam.sheldon@hotmail.com>
2021-04-22 16:13:17 -0700

    [Feature] List accounts on accounts page.

--- client/src/pages/Accounts.tsx
+++ client/src/pages/Accounts.tsx
@@ -1,21 +1,44 @@
 import React from "react";
-
+import { Container, Grid } from "@material-ui/core";
 import { makeStyles, createStyles, Theme } from "@material-ui/core/styles";
-import Paper from "@material-ui/core/Paper";
-import Grid from "@material-ui/core/Grid";
+import { useQuery } from "@apollo/client";
+import Queries from "../graphql/Queries";
+import { getAllAccounts } from "../graphql/types/getAllAccounts";
+import AccountView from "../components/views/AccountView";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
-    root: {
+    accountsRoot: {
       padding: 30,
       paddingTop: 80,
       flexGrow: 1,
+      boxSizing: "border-box",
     },
   })
 );
 
 export default function Accounts() {
   const classes = useStyles();
+  const { data } = useQuery<getAllAccounts>(Queries.GET_ALL_ACCOUNTS);
 
-  return <div className={classes.root}>Accounts</div>;
+  if (!data?.allAccounts) {
+    return (
+      <Container className={classes.accountsRoot}>
+        Loading accounts...
+      </Container>
+    );
+  }
+
+  return (
+    <Container className={classes.accountsRoot}>
+      <Grid container>
+        {data?.allAccounts.map((account) => (
+          <Grid item>
+            <AccountView account={account} />
+          </Grid>
+        ))}
+        <Grid item>{/* todo create account flow */}</Grid>
+      </Grid>
+    </Container>
+  );
 }
