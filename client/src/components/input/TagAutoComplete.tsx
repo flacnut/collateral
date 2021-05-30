bcdf13b165638a441af70d1ed3229f79a7f5349f
flacnut <adam.sheldon@hotmail.com>
2021-05-29 21:34:12 -0700

    [Feature] Add unstyled tag selector / editor.

---
+++ client/src/components/input/TagAutoComplete.tsx
@@ -0,0 +1,71 @@
+/* eslint-disable no-use-before-define */
+import React from "react";
+import Chip from "@material-ui/core/Chip";
+import TextField from "@material-ui/core/TextField";
+import Autocomplete from "@material-ui/lab/Autocomplete";
+
+type Tag = {
+  id: number;
+  tag: string;
+  fixed?: boolean;
+};
+
+type Props = {
+  id: string;
+  initialValue: Tag[];
+  options: Tag[];
+  onChange: (newValue: Tag[]) => void;
+};
+
+export default function TagAutoComplete(props: Props) {
+  const isUnsavedTag = (tag: Tag) => {
+    return (
+      props.initialValue.filter((initialTag) => initialTag.id === tag.id)
+        .length === 0
+    );
+  };
+
+  const isFixedTag = (tag: Tag) => {
+    return (
+      props.options.find((optionTag) => optionTag.id === tag.id)?.fixed === true
+    );
+  };
+
+  const fixedOptions = props.initialValue.filter(isFixedTag);
+  const [value, setValue] = React.useState([
+    ...fixedOptions,
+    ...props.initialValue.filter((tag) => !isFixedTag(tag)),
+  ]);
+
+  return (
+    <Autocomplete
+      multiple
+      id="fixed-tags-demo"
+      value={value}
+      onChange={(event, newValue) => {
+        setValue([
+          ...fixedOptions,
+          ...newValue.filter((tag) => !isFixedTag(tag)),
+        ]);
+      }}
+      popupIcon={null}
+      options={props.options}
+      getOptionLabel={(option) => option.tag}
+      renderTags={(tagValue, getTagProps) =>
+        tagValue.map((option, index) => (
+          <Chip
+            label={option.tag}
+            {...getTagProps({ index })}
+            disabled={fixedOptions.indexOf(option) !== -1}
+            variant={isUnsavedTag(option) ? "outlined" : "default"}
+            {...(isFixedTag(option) ? { onDelete: undefined } : {})}
+          />
+        ))
+      }
+      style={{ width: 500 }}
+      renderInput={(params) => (
+        <TextField {...params} label="" variant="standard" placeholder="" />
+      )}
+    />
+  );
+}
--- client/src/pages/Library.tsx
+++ client/src/pages/Library.tsx
@@ -4,6 +4,7 @@ import CSVDropZone from "../components/input/CSVDropZone";
 import { makeStyles, createStyles, Theme } from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
+import TagAutoComplete from "../components/input/TagAutoComplete";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -20,6 +21,31 @@ const useStyles = makeStyles((theme: Theme) =>
   })
 );
 
+// example tags
+const exampleTags = [
+  { id: 1, tag: "salary", fixed: true },
+  { id: 2, tag: "asset-purchase" },
+  { id: 3, tag: "asset-sale" },
+  { id: 4, tag: "expense", fixed: true },
+  { id: 5, tag: "investment" },
+  { id: 6, tag: "one-off" },
+  { id: 7, tag: "income" },
+  { id: 8, tag: "car" },
+  { id: 9, tag: "fitness" },
+  { id: 10, tag: "transfer" },
+  { id: 11, tag: "Amazon" },
+  { id: 12, tag: "QFC" },
+  { id: 13, tag: "Groceries" },
+  { id: 14, tag: "PCC" },
+  { id: 15, tag: "Haggens" },
+  { id: 16, tag: "Wholefoods" },
+  { id: 17, tag: "MetropolitanMarket" },
+  { id: 18, tag: "Yuliya", fixed: true },
+  { id: 19, tag: "Adam", fixed: true },
+  { id: 20, tag: "Costco" },
+  { id: 21, tag: "cc-payment" },
+];
+
 export default function Library() {
   const classes = useStyles();
 
@@ -28,6 +54,18 @@ export default function Library() {
       <Grid container spacing={2}>
         <Grid item xs={12}>
           <CSVDropZone onSaveFile={(f) => true} />
+        </Grid>
+        <Grid item xs={12}>
+          <TagAutoComplete
+            id="tags-autocomplete-example"
+            options={exampleTags}
+            onChange={(t) => console.dir(t)}
+            initialValue={[
+              { id: 4, tag: "expense" },
+              { id: 19, tag: "Adam" },
+              { id: 20, tag: "Costco" },
+            ]}
+          />
         </Grid>
         <Grid item xs={12} sm={6}>
           <Paper className={classes.paper}>xs=12 sm=6</Paper>
