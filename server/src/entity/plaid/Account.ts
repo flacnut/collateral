19d3e9f819c0a936dfabe4d45a9e924eb8dc3149
Adam Sheldon <adam.sheldon@hotmail.com>
2022-12-05 22:03:05 +1100

    [server] Account now returns transactions

--- server/src/entity/plaid/Account.ts
+++ server/src/entity/plaid/Account.ts
@@ -1,11 +1,13 @@
-import { Field, ObjectType } from "type-graphql";
-import { Entity, BaseEntity, Column, PrimaryColumn, ManyToOne, AfterLoad } from "typeorm";
+import { Arg, Field, Int, ObjectType } from "type-graphql";
+import { Entity, BaseEntity, Column, PrimaryColumn, ManyToOne } from "typeorm";
 import { PlaidAccountBalance } from "./Balance";
+import { CoreTransaction } from "./CoreTransaction";
 import { PlaidItem } from "./Item";
 
 @Entity("plaid_account")
 @ObjectType()
 export class PlaidAccount extends BaseEntity {
+  // Core Plaid Fields
 
   @Field()
   @PrimaryColumn("text", { nullable: false, unique: true })
@@ -43,18 +45,41 @@ export class PlaidAccount extends BaseEntity {
   @Column("text", { nullable: true })
   currency: string | null;
 
+  // Additional Fields
+
   @ManyToOne(() => PlaidItem, (item) => item.accounts)
   item: PlaidItem;
 
   @Field(() => PlaidAccountBalance)
-  latestBalance: PlaidAccountBalance | null;
-
-  @AfterLoad()
-  async setLatestBalance() {
+  async latestBalance() {
     const balances = await PlaidAccountBalance.find({
-      accountId: this.id });
-    this.latestBalance = balances
+      accountId: this.id
+    });
+    return balances
       .sort((a, b) => (new Date(a.lastUpdateDate)).getTime() - (new Date(b.lastUpdateDate)).getTime())
       .pop() ?? null;
+  }
+
+  @Field(() => Int)
+  async totalTransactions(): Promise<number> {
+    const [_, count] = await CoreTransaction.findAndCount({ accountId: this.id });
+    return count;
+  }
+
+  @Field(() => [CoreTransaction])
+  async transactions(@Arg("after", { nullable: true }) after: number): Promise<CoreTransaction[]> {
+    const results = await CoreTransaction.getRepository().createQueryBuilder("")
+      .where("accountId = :id", { id: this.id })
+      .orderBy("date", "DESC")
+      .offset(after)
+      .limit(10)
+      .execute();
+
+    return results.map((result: { [key: string]: any }) => {
+      let t = {} as { [key: string]: any };
+      // Remove CoreTransaction_ prefix to cast
+      Object.keys(result).forEach(key => t[key.split('_')[1]] = result[key]);
+      return t as CoreTransaction;
+    })
   }
 }
