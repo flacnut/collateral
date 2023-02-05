94acc985f8cc4c3e1d732951e533b52dc406d539
Adam Sheldon <adam.sheldon@hotmail.com>
2023-02-05 00:33:29 -0800

    [cleanup] DELETE old Clients!! No going back!

--- client/src/components/grids/index.ts
+++
@@ -1,6 +0,0 @@
-import { TransactionGrid } from "./TransactionGrid";
-import { TransactionDataGrid } from "./TransactionDataGrid";
-import { SelectableTransactionGrid } from "./SelectableTransactionGrid";
-import { AccountsGrid } from "./AccountsGrid";
-
-export { TransactionGrid, TransactionDataGrid, SelectableTransactionGrid, AccountsGrid };
