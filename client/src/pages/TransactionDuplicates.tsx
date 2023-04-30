146c66a7c247a1081f982512d8e1d25b379ebd73
Adam Sheldon <adam.sheldon@hotmail.com>
2023-04-30 11:42:37 -0700

    [client][duplicates] Add getDuplicates query.

--- client/src/pages/TransactionDuplicates.tsx
+++ client/src/pages/TransactionDuplicates.tsx
@@ -1,9 +1,31 @@
+import { useLazyQuery } from '@apollo/client';
 import { Container, Typography } from '@mui/material';
+import { useEffect } from 'react';
 import { Helmet } from 'react-helmet-async';
 import { useSettingsContext } from 'src/components/settings';
+import { gql } from 'src/__generated__';
+
+const getDuplicatesQuery = gql(`
+query getDuplicates($accountId: String) {
+  getDuplicates(accountId:$accountId) {
+    key
+    transactions {
+      id
+      amountCents
+    }
+  }
+}`);
 
 export default function TransactionDuplicates() {
   const { themeStretch } = useSettingsContext();
+
+  const [fetchDuplicates, fetchDuplicatesResult] = useLazyQuery(getDuplicatesQuery);
+
+  useEffect(() => {
+    fetchDuplicates({
+      variables: {},
+    });
+  }, []);
 
   return (
     <>
@@ -15,6 +37,8 @@ export default function TransactionDuplicates() {
         <Typography variant="h3" component="h1" paragraph>
           Transaction Duplicates
         </Typography>
+
+        <pre>{JSON.stringify(fetchDuplicatesResult.data?.getDuplicates)}</pre>
       </Container>
     </>
   );
