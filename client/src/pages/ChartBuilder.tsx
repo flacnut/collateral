ef063587520dfecc46199601b2bd09057363e9b9
Adam Sheldon <adam.sheldon@hotmail.com>
2023-05-20 13:11:53 -0700

    [client][chartbuilder] Stub out ChartBuilder.tsx.

---
+++ client/src/pages/ChartBuilder.tsx
@@ -0,0 +1,20 @@
+import { Container, Typography } from '@mui/material';
+import { Helmet } from 'react-helmet-async';
+import { useSettingsContext } from 'src/components/settings';
+
+export default function ChartBuilder() {
+  const { themeStretch } = useSettingsContext();
+  return (
+    <>
+      <Helmet>
+        <title>Transaction: Chartify</title>
+      </Helmet>
+
+      <Container maxWidth={themeStretch ? false : 'xl'}>
+        <Typography variant="h3" component="h1" paragraph>
+          Transaction Charts
+        </Typography>
+      </Container>
+    </>
+  );
+}
