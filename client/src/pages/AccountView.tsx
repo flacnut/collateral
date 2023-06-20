b425697764325780cf632642854b4757d156097f
Adam Sheldon <adam.sheldon@hotmail.com>
2023-06-19 21:38:56 -0700

    [client][accounts] Initial GQL for fetching and rendering account data.

--- client/src/pages/AccountView.tsx
+++ client/src/pages/AccountView.tsx
@@ -3,39 +3,72 @@ import { Helmet } from 'react-helmet-async';
 import { Container, Typography } from '@mui/material';
 // components
 import { useSettingsContext } from '../components/settings';
+import { useParams } from 'react-router';
+import { useLazyQuery } from '@apollo/client';
+import { useEffect } from 'react';
+import { gql } from 'src/__generated__/gql';
 
 // ----------------------------------------------------------------------
 
+const getAccountQuery = gql(`
+query getAccount($accountId: String!) {
+  getAccount(accountId: $accountId) {
+    name
+    officialName
+    status
+    type
+    latestTransaction {
+      id
+      amount
+      amountCents
+      classification
+      date
+      description
+      tags {
+        name
+      }
+    }
+    latestBalance {
+      availableCents
+      balanceCents
+      lastUpdateDate
+      limitCents
+    }
+    institution {
+      logo
+      primaryColor
+      name
+    }
+  }
+}`);
+
 export default function AccountView() {
   const { themeStretch } = useSettingsContext();
+  const { id } = useParams<string>();
+  const [refetch, { data, loading }] = useLazyQuery(getAccountQuery);
+
+  useEffect(() => {
+    !!id &&
+      refetch({
+        variables: {
+          accountId: id,
+        },
+      });
+  }, [id]);
 
   return (
     <>
       <Helmet>
-        <title> Account</title>
+        <title>Account</title>
       </Helmet>
 
       <Container maxWidth={themeStretch ? false : 'xl'}>
         <Typography variant="h3" component="h1" paragraph>
-          Page Five
+          Account
         </Typography>
 
         <Typography gutterBottom>
-          Curabitur turpis. Vestibulum facilisis, purus nec pulvinar iaculis, ligula mi congue nunc,
-          vitae euismod ligula urna in dolor. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit
-          id, lorem. Phasellus blandit leo ut odio. Vestibulum ante ipsum primis in faucibus orci
-          luctus et ultrices posuere cubilia Curae; Fusce id purus. Aliquam lorem ante, dapibus in,
-          viverra quis, feugiat a, tellus. In consectetuer turpis ut velit. Aenean posuere, tortor
-          sed cursus feugiat, nunc augue blandit nunc, eu sollicitudin urna dolor sagittis lacus.
-          Vestibulum suscipit nulla quis orci. Nam commodo suscipit quam. Sed a libero.
-        </Typography>
-
-        <Typography>
-          Praesent ac sem eget est egestas volutpat. Phasellus viverra nulla ut metus varius
-          laoreet. Curabitur ullamcorper ultricies nisi. Ut non enim eleifend felis pretium feugiat.
-          Donec mi odio, faucibus at, scelerisque quis, convallis in, nisi. Fusce vel dui. Quisque
-          libero metus, condimentum nec, tempor a, commodo mollis, magna. In enim justo, rhoncus ut,
-          imperdiet a, venenatis vitae, justo. Cras dapibus.
+          <pre>${JSON.stringify(data?.getAccount, null, 2)}</pre>
         </Typography>
       </Container>
     </>
